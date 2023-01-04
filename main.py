f6a64d05ddfe6c758928fff12d6af471d98a64f9
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-01-04 13:38:22 +0000

    hi

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-import tkinter as tk
+from tkinter import *
+from datetime import date 
 
-window = tk.Tk()
-window.title("Hello wold")
-window.geometry("300x300")
+#importing font
+#import pyglet
+#pyglet.font.add_file('font name')
 
-hello = tk.Label(text="Hello world!")
-hello.pack()
-button = tk.Button(text="Click me!")
-button.pack()
+bc = "#ffffff"
+fc = "#e34646"
 
-tk.mainloop()
+#window configuration
+window = Tk()
+window.title("Digital Clock")
+window.geometry('485x180')
+window.resizable(width=False, height=False)
+window.configure(bg=bc)
+
+ def clock(): 
+  time = datetime.now  
+  hour = time.strftime("%H : %M : %S)
+  print(hour)
+
+
+
+l1 = Label(window, text = "12:49:05", font=('Arial 80'), bg=bc)
+l1.grid(row = 0, column = 0, padx = 5, sticky = NW)
+
+l2 = Label(window, text = "Wednesday 04/01/2023", font=('Arial 20'), bg=bc)
+l2.grid(row = 1, column = 0, padx = 5, sticky = NW)
+
+window.mainloop()
